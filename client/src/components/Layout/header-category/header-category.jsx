7b53a4b249dbcf403e6ce32bd0b3d53a6f5cb6f5
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2023-08-27 23:55:34 +0100

    create signup form

---
+++ client/src/components/Layout/header-category/header-category.jsx
@@ -0,0 +1,47 @@
+import Header_link from './header-category-link';
+
+function Header_Category({ category }) {
+    const menCategory = () => {
+        return (
+            <section id="header_category-container">
+                <Header_link text="New In" />
+                <Header_link text="Clothing" />
+                <Header_link text="Shoes" />
+                <Header_link text="Accessories" />
+                <Header_link text="SportsWear" />
+                <Header_link text="Summer" />
+                <Header_link text="Brands" />
+                <Header_link text="TopMan" />
+                <Header_link text="Marketplace" />
+                <div className="parallelogram flex h-full w-20 items-center justify-center bg-red-800 font-semibold ">
+                    <Header_link text="Outlet" />
+                </div>
+            </section>
+        );
+    };
+    const womenCategory = () => {
+        return (
+            <section id="header_category-container">
+                <Header_link text="New In" />
+                <Header_link text="Clothing" />
+                <Header_link text="Dresses" />
+                <Header_link text="Shoes" />
+                <Header_link text="Accessories" />
+                <Header_link text="Face + Body" />
+                <Header_link text="TopShop" />
+                <Header_link text="SportsWear" />
+                <Header_link text="Summer" />
+                <Header_link text="Brands" />
+<Header_link text="Marketplace" />
+                <div className="parallelogram flex h-full w-20 items-center justify-center bg-red-800 font-semibold ">
+                    <Header_link text="Outlet" />
+                </div>
+                
+            </section>
+        );
+    };
+
+    return <>{category === 'men' ? menCategory() : womenCategory()}</>;
+}
+
+export default Header_Category;
--- client/src/components/Layout/header.jsx
+++ client/src/components/Layout/header.jsx
@@ -6,7 +6,12 @@ import heart from '../../assets/heart.png';
 import Dropdown_Hover from '../common/dropdown/dropdown_hover';
 import '../../CSS/dropdown.css';
 import Profile_Dropdown from '../common/dropdown/profile_dropdown';
+import Header_Category from './header-category/header-category';
+import { useState } from 'react';
 function Header() {
+    const [category, setCategory] = useState('men');
+    //const [activeCategory, setActiveCategory] = useState(false)
+
     const search = (e) => {
         console.log(e);
     };
@@ -30,55 +35,76 @@ function Header() {
         </div>,
     ];
     return (
-        <header className="header">
-            <div id="image-wrapper" className="image-wrapper">
-                <a href="/">
-                    <img src={logo} alt="glamo logo" className="" />
-                </a>
-            </div>
-            <section id="women">Women</section>
-            <section id="men">Men</section>
-            <section id="search-input-section">
-                <input
-                    type="text"
-                    id="search-input"
-                    onChange={search}
-                    placeholder="Search for items"
-                />
-                <div id="search-icon-section">
-                    <img src={search_icon} alt="search icon" id="search-icon" />
+        <>
+            <header className="header">
+                <div id="image-wrapper" className="image-wrapper">
+                    <a href="/">
+                        <img src={logo} alt="glamo logo" className="" />
+                    </a>
                 </div>
-            </section>
+                <section
+                    id="women"
+                    onClick={() => setCategory('women')}
+                    className={
+                        category == 'women' ? 'active-header-category' : null
+                    }
+                >
+                    Women
+                </section>
+                <section
+                    id="men"
+                    onClick={() => setCategory('men')}
+                    className={
+                        category == 'men' ? 'active-header-category' : null
+                    }
+                >
+                    Men
+                </section>
+                <section id="search-input-section">
+                    <input
+                        type="text"
+                        id="search-input"
+                        onChange={search}
+                        placeholder="Search for items"
+                    />
+                    <div id="search-icon-section">
+                        <img
+                            src={search_icon}
+                            alt="search icon"
+                            id="search-icon"
+                        />
+                    </div>
+                </section>
 
-            <section id="icons-wrapper" className="h-full">
-                
+                <section id="icons-wrapper" className="h-full">
                     <Dropdown_Hover
                         button={profile}
                         dropdown_options={<Profile_Dropdown />}
                     />
-                
 
-                <div className="header-icon">
-                <a href='/wishlist' className='w-full h-full'>
-                    <img
-                        src={heart}
-                        alt="heart icon"
-                        className="img-icon"
-                    ></img>
-                    </a>
-                </div>
-                <div className="header-icon" >
-                    <a href='/cart' className='w-full h-full'>
-                      <img
-                        src={basket}
-                        alt="basket icon"
-                        className="img-icon "
-                    ></img>  
-                    </a>
-                    
-                </div>
-            </section>
-        </header>
+                    <div className="header-icon">
+                        <a href="/wishlist" className="h-full w-full">
+                            <img
+                                src={heart}
+                                alt="heart icon"
+                                className="img-icon"
+                            ></img>
+                        </a>
+                    </div>
+                    <div className="header-icon">
+                        <a href="/cart" className="h-full w-full">
+                            <img
+                                src={basket}
+                                alt="basket icon"
+                                className="img-icon "
+                            ></img>
+                        </a>
+                    </div>
+                </section>
+            </header>
+            {category=="men" ? (<Header_Category category={"men"}/>) : (<Header_Category category={"women"}/>)}
+            
+        </>
     );
 }
 
