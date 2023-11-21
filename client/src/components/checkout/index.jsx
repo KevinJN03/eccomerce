fe71aa19a1f09d0b1264c725f8d808f932debb25
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2023-11-21 22:07:04 +0000

    update address form

--- client/src/components/checkout/index.jsx
+++ client/src/components/checkout/index.jsx
@@ -24,9 +24,41 @@ function Checkout() {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
     const [isOrderSubmit, setOrderSubmit] = useState(false);
-
-    const [shippingAddress, setShippingAddress] = useState(exampleCustomerInfo);
+    const [addresses, setAddresses] = useState([]);
+    const [shippingAddress, setShippingAddress] = useState({});
     const [billingAddress, setBillingAddress] = useState(exampleCustomerInfo);
+    const [defaultAddresses, setDefaultAddresses] = useState({});
+    useEffect(() => {
+        axios
+            .get('user/userData')
+            .then((res) => {
+                const { user } = res.data;
+
+                const default_address = user?.default_address;
+                setDefaultAddresses(() => user?.default_address || {});
+                const findAddress = (property, setState) => {
+                    if (default_address[property]) {
+                        const foundAddress = user.address.find(
+                            (item) => item._id == default_address[property]
+                        );
+
+                        console.log({ foundAddress });
+                        setState(() => foundAddress);
+                    }
+                };
+
+                findAddress('shipping_address', setShippingAddress);
+                findAddress('billing_address', setBillingAddress);
+
+                setAddresses(() => user.address);
+            })
+            .catch((error) => {
+                console.error(
+                    'error while trying to get logged in user data',
+                    error
+                );
+            });
+    }, []);
     const { cart } = useCart();
     useEffect(() => {
         if (cart.length == 0) {
@@ -96,6 +128,9 @@ function Checkout() {
                                 <Address
                                     shippingAddress={shippingAddress}
                                     setShippingAddress={setShippingAddress}
+                                    addresses={addresses}
+                                    defaultAddresses={defaultAddresses}
+                                    defaultProperty={'shipping_address'}
                                 />
                                 <Delivery />
                                 <Payment
