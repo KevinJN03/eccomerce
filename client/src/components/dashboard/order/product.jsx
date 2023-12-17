266f81e83a04e041930277f3702bc55f451ea87f
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2023-12-17 15:17:43 +0000

    updateuser dashboard page

--- client/src/components/dashboard/order/product.jsx
+++ client/src/components/dashboard/order/product.jsx
@@ -1,22 +1,27 @@
-function Product({ img, title, price, variation1, variation2 }) {
+import { Link } from "react-router-dom";
+
+function Product({ img, title, price, variation1, variation2, id }) {
     return (
-        <div className="flex flex-col">
-            <div className="w-44 hover:underline">
-                <img className="mt-6 h-56 w-44 object-cover" src={img || ''} />
-
-                <p className="mt-4 w-44">{title}</p>
+        <div className="flex h-full flex-col w-40">
+            <div className="w-full hover:underline mb-4">
+                <img className="mt-6 h-56 w-full object-cover" src={img || ''} />
             </div>
+            <div className="h-full">
+                <p className="w-full text-s hover:underline text-ellipsis overflow-hidden !h-10 mb-4"><Link target="_blank" to={`/product/${id}`}>
+                    {title}
+                    </Link></p>
 
-            <p className="py-6 font-gotham text-base tracking-widest text-primary">
-                {`£${parseFloat(price).toFixed(2) || ''}`}
-            </p>
+                <p className="py-0  h-8 font-gotham text-base tracking-widest text-primary">
+                    {`£${parseFloat(price).toFixed(2) || ''}`}
+                </p>
 
-            <p className="w-44 border-y-[1px] py-3 text-sm font-light">
-                {variation1}
-            </p>
-            {variation2 && (
-                <p className="mb-6 border-b-[1px] py-3">{variation2}</p>
-            )}
+                <p className="w-full border-y-[1px] py-3 text-sm font-light h-10">
+                    {variation1}
+                </p>
+                {variation2 && (
+                    <p className="mb-6 border-b-[1px] py-3 h-10">{variation2}</p>
+                )}
+            </div>
         </div>
     );
 }
