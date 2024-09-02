2d2cc7a41a20d99274271710ac0c4bedd15dc500
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2024-09-02 07:14:41 +0100

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
-
+const { VITE_SENTRY_PROJECT, VITE_SENTRY_ORG, VITE_SENTRY_AUTH_TOKEN } =
+    import.meta.env;
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
@@ -11,8 +12,10 @@ export default defineConfig({
         mkcert({ autoUpgrade: true, savePath: './cert/' }),
         svgr(),
         sentryVitePlugin({
-            org: 'kevin-jean',
-            project: 'javascript-react',
+            org: VITE_SENTRY_ORG,
+            project: VITE_SENTRY_PROJECT,
+            authToken: VITE_SENTRY_AUTH_TOKEN,
+            telemetry: false,
         }),
     ],
 
